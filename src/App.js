aceceea0f74bbf8f7dfe5e136864a14cea04c3d2
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-06-01 17:48:52 -0500

    Configurando rutas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
-import HeadlineCards from "./components/HeadlineCards";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Category from "./components/Category";
 import Food from "./components/Food";
+import HeadlineCards from "./components/HeadlineCards";
+import ShoppingCart from "./components/ShoppingCart";
+import Category from "./components/Category";
 
 function App() {
-
   return (
-    <div>
-        <Navbar/>
-        <Hero/>
-        <HeadlineCards/>
-        <Food/>
-        <Category/>
-    </div>
-
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Hero />
+              <Food />
+              <HeadlineCards />
+              <Category />
+            </div>
+          } />
+          <Route path="/shoppingCart" element={<ShoppingCart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ShoppingCart from './ShoppingCart'
 
 const Hero = () => {
@@ -13,10 +13,11 @@ const Hero = () => {
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'> <span className='text-orange-500'>Comida</span> A la mano</h1>
             </div>
             <div>
-              <img className='w-full max-h-[500px] object-cover' src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="/" />
-              <Routes>
-              <Route path='/shoppingCart' element={<ShoppingCart/>}></Route>
-              </Routes>
+            <Link to='/shoppingCart'>
+              <img className='w-full max-h-[500px] object-cover' 
+                src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                alt="/" />
+            </Link>
             </div>
         </div>
     </div>
